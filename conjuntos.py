3a073d124e30fa53f7f12f8f672808bb3402e2b4
Miguel-Rios1094 <164534109+Miguel-Rios1094@users.noreply.github.com>
2024-09-06 12:22:05 -0600

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,8 @@
+#Crear Conjunto
+Alumno = {"nombre", "edad", "escuela"}
+
+print(Alumno)
+
+#Conjunto con ciclo for
+for Escuela in Alumno:
+    print(Escuela)
---
+++ diccionario.py
@@ -0,0 +1,12 @@
+#Creando diccionario
+Escuela = {
+  "Nombre": "Miguel",
+  "Especialidad": "Programacion",
+  "Grupo":5,
+  "Grado":"I"
+}
+print(Escuela)
+
+#Diccionario con ciclo For
+for x in Escuela:
+  print(x)
